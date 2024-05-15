9c5bdbda27f088191eb8f8b60b93aa1bf5453e45
Eldarfox <168174614+Eldarfox@users.noreply.github.com>
2024-05-15 17:59:00 +0600

    Add files via upload

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -16,15 +16,61 @@ gmailButton.onclick = () => {
 
 
 const childBlock = document.querySelector(".child_block")
+const parentBlock = document.querySelector(".parent_block")
 
 let positionX = 0
+let positionY = 0
+
+const maxParentWidth = parentBlock.offsetWidth - childBlock.offsetWidth
+const maxParentHeight = parentBlock.offsetHeight - childBlock.offsetHeight
 
 const move = () => {
-    if (positionX < 449){
+    if (positionX < maxParentHeight && positionY === 0){
         positionX++
         childBlock.style.left = `${positionX}px`
         requestAnimationFrame(move)
+    }else if (positionX >= maxParentWidth &&  positionY < maxParentHeight){
+        positionY++
+        childBlock.style.top = `${positionY}px`
+        requestAnimationFrame(move)
+    }else if (positionX > 0 && positionY > 0) {
+        positionX--
+        childBlock.style.left = `${positionX}px`
+        requestAnimationFrame(move)
+    }else if (positionX === 0 && positionY > 0) {
+        positionY--
+        childBlock.style.top = `${positionY}px`
+        requestAnimationFrame(move)
     }
 }
-
 move()
+
+const start = document.querySelector("#start")
+const stop = document.querySelector("#stop")
+const reset = document.querySelector("#reset")
+const seconds = document.querySelector("#seconds")
+
+let second = 0
+let interval;
+let work = false;
+const time = () => {
+    second++
+    seconds.innerHTML = `${second}`
+}
+start.onclick = () => {
+    if (work === false) {
+    interval = setInterval(time, 1000)
+    work = true;}
+}
+
+stop.onclick = () => {
+    clearInterval(interval)
+    work = false
+}
+
+reset.onclick = () => {
+    clearInterval(interval)
+    second = 0
+    seconds.innerHTML = "0"
+    work = false
+}
---
+++ project/js/modal.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal")
+const modalTrigger = document.querySelector("#btn-get")
+const modalCloseBtn = document.querySelector(".modal_close")
+const openModal = () => {
+    modal.style.display = "block"
+    document.body.style.overflow = "hidden"
+}
+
+const closeModal = () => {
+    modal.style.display = "none"
+    document.body.style.overflow = ""
+}
+
+modalTrigger.onclick = openModal
+modalCloseBtn.onclick = closeModal
+
+modal.onclick = () => {
+    if (event.target === modal){
+    closeModal()}
+}
